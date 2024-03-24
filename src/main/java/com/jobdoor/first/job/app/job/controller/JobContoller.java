26c816f9b62d92d4531a0cdaad5a355550613757
Niket <niketshah2010@gmail.com>
2024-03-24 15:45:52 +0530

    Job Controller and services

--- src/main/java/com/jobdoor/first/job/app/job/controller/JobContoller.java
+++ src/main/java/com/jobdoor/first/job/app/job/controller/JobContoller.java
@@ -3,6 +3,8 @@ package com.jobdoor.first.job.app.job.controller;
 import com.jobdoor.first.job.app.job.bean.Job;
 import com.jobdoor.first.job.app.job.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -14,19 +16,21 @@ public class JobContoller {
         @Autowired
         private JobService jobService;
         @GetMapping("/jobs")
-        public List<Job> findAll(){
-               return jobService.findAll();
+        public ResponseEntity< List<Job>> findAll(){
+               return ResponseEntity.ok(jobService.findAll());
         }
 
         @GetMapping ("/job/{id}")
-        public Job findByID(@PathVariable("id") Long id){
-                return jobService.findById(id);
+        public ResponseEntity<Job> findByID(@PathVariable("id") Long id){
+                Job job=jobService.findById(id);
+                if(null!=job) return new ResponseEntity<Job>(job,HttpStatus.OK);
+                return new ResponseEntity<Job>(job,HttpStatus.NOT_FOUND);
         }
 
         @PostMapping("/addjob")
-        public String addJob(@RequestBody Job job){
+        public  ResponseEntity<String> addJob(@RequestBody Job job){
                 jobService.createJob(job);
-                return  "Job added Successfully";
+                return new ResponseEntity<>( "Job added Successfully",HttpStatus.OK);
         }
 
 }
